b656ca9ca99191715fb305620f34533632f78b07
Bwvdhelm <bvanderh@gmail.com>
2019-03-26 22:47:38 +0100

    feat(styling): starting with material palette

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryOverviewComponent } from './category-overview/category-overview.component';
+import { CategoryComponent } from './category-overview/category/category.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeWidgetComponent } from './homepage/home-widget/home-widget.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { CategoryComponent } from './category-overview/category/category.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { CategoryComponent } from './category-overview/category/category.compone
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
