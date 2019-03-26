3f637891684c1559c46148f14e4d0ce72e78c7a4
Bwvdhelm <bvanderh@gmail.com>
2019-03-26 18:46:00 +0100

    feat(homewidget): Input styling

--- src/app/homepage/home-widget/home-widget.component.ts
+++ src/app/homepage/home-widget/home-widget.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-home-widget',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-widget.component.scss']
 })
 export class HomeWidgetComponent implements OnInit {
+
+  @ViewChild('input') input: MatInput;
+
+  PLACEHOLDER = 'FILL IN YOUR FLIGHT NUMBER';
 
   constructor() { }
 
