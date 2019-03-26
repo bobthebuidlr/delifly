cd04d64426a60889bb64493bd4fca9b27e0d8ad1
Bwvdhelm <bvanderh@gmail.com>
2019-03-26 07:38:28 +0100

    feat(home): Created widget, footer, background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeWidgetComponent } from './homepage/home-widget/home-widget.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    FooterComponent,
+    HomeWidgetComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, MatButtonModule, MatInputModule, MatFormFieldModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
