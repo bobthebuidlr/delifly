a3b7f297f246de2023378d264d685ea71a0055f7
Bwvdhelm <bvanderh@gmail.com>
2019-04-17 19:29:45 +0200

    fix(texts): changes text inquiry, trimmed home video

--- src/app/gallery/no-products/no-products.component.ts
+++ src/app/gallery/no-products/no-products.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NoProductsComponent implements OnInit {
 
   buttonText = 'Inquire'
-  headerText = 'Inquire about your Personal Styling Concierge now'
+  headerText = 'Inquire about your Personal Styling Concierge'
 
   constructor() { }
 
